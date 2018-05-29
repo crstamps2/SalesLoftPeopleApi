5e157dd258e2756ff3d2e67561d3bfd0ee4ac56e
Cody Stamps <crstamps2@gmail.com>
2018-05-28 23:11:12 -0400

    fix broken test

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,3 +1,5 @@
+require "awrence"
+
 class PeopleController < ApplicationController
   def index
     render json: PeopleService.get.to_camelback_keys
--- spec/controllers/people_controller_spec.rb
+++ spec/controllers/people_controller_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe PeopleController, type: :controller do
   describe 'GET #index' do
     before do
-      allow(PeopleService).to receive(:get).and_return('{"blah":"blah"}')
+      allow(PeopleService).to receive(:get).and_return({"blah":"blah"})
     end
 
     it 'returns back a list of people' do
