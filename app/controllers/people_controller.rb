eff0552ac406d47687dddf113c93d48db8999a5d
Cody Stamps <crstamps2@gmail.com>
2018-05-29 09:38:11 -0400

    add an endpoint that returns a breakdown of all people’s emails
    and add tests.

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -4,4 +4,11 @@ class PeopleController < ApplicationController
   def index
     render json: PeopleService.get.to_camelback_keys
   end
+
+  def allEmailBreakdown
+    people = PeopleService.get.to_camelback_keys
+    emails = people[:data].map { |person| person[:emailAddress] }
+    render json: EmailBreakdownService.get_breakdown_of_emails(emails)
+  end
+
 end
--- spec/controllers/people_controller_spec.rb
+++ spec/controllers/people_controller_spec.rb
@@ -11,4 +11,32 @@ RSpec.describe PeopleController, type: :controller do
       expect(response.body).to eq '{"blah":"blah"}'
     end
   end
+
+  describe 'GET #allEmailBreakdown' do
+    let(:people) do
+      {
+        "data": [
+          {
+            "emailAddress": "hey@hey.net",
+          },
+          {
+            "emailAddress": "foo@bar.com",
+          },
+          {
+            "emailAddress": "fiz@bah.org",
+          }
+        ]
+      }
+    end
+    before do
+      allow(PeopleService).to receive(:get).and_return(people)
+    end
+
+    it 'returns a breakdown of the number of each characters combined in email of all People' do
+      get :allEmailBreakdown
+      expected = "{\"h\":3,\"e\":3,\"y\":2,\"@\":3,\".\":3,\"n\":1,\"t\":1,\"f\":2,\"o\":4,\"b\":2,\"a\":2,\"r\":2,\"c\":1,\"m\":1,\"i\":1,\"z\":1,\"g\":1}"
+      expect(response.body).to eq expected
+    end
+  end
+
 end
